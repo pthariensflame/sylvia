8e11985f36f8936bf3611371e33dc22f6f3d8605
Alexander Ronald Altman <alexanderaltman@me.com>
2020-04-26 22:37:08 -0700

    DeclarationNode

--- src/main/kotlin/com/pthariensflame/sylvia/ast/declarations/DeclarationNode.kt
+++ src/main/kotlin/com/pthariensflame/sylvia/ast/declarations/DeclarationNode.kt
@@ -1,4 +1,4 @@
-package com.pthariensflame.sylvia.ast.statements
+package com.pthariensflame.sylvia.ast.declarations
 
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary
 import com.oracle.truffle.api.dsl.GenerateNodeFactory
@@ -11,6 +11,7 @@ import com.oracle.truffle.api.nodes.NodeInfo
 import com.oracle.truffle.api.source.Source
 import com.oracle.truffle.api.source.SourceSection
 import com.pthariensflame.sylvia.ast.SylviaNode
+import com.pthariensflame.sylvia.ast.statements.StatementNode
 import com.pthariensflame.sylvia.parser.SourceSpan
 import com.pthariensflame.sylvia.parser.createSection
 
@@ -22,22 +23,22 @@ import com.pthariensflame.sylvia.parser.createSection
 @GenerateWrapper
 @GenerateUncached(inherit = true)
 @Introspectable
-abstract class StatementNode
+abstract class DeclarationNode
 @JvmOverloads constructor(
-    @JvmField val srcSpan: SourceSpan? = null,
-) : Node(), SylviaNode, InstrumentableNode {
+    srcSpan: SourceSpan? = null,
+) : StatementNode(srcSpan), SylviaNode, InstrumentableNode {
     abstract override fun isInstrumentable(): Boolean
 
     override fun createWrapper(probe: ProbeNode): InstrumentableNode.WrapperNode =
-        StatementNodeWrapper(this, probe)
+        DeclarationNodeWrapper(this, probe)
 
-    abstract fun executeVoid(frame: VirtualFrame)
+    abstract override fun executeVoid(frame: VirtualFrame)
 
     override fun hasTag(tag: Class<out Tag>): Boolean =
         tag.kotlin == StandardTags.StatementTag::class || super.hasTag(tag)
 
     @GenerateWrapper.OutgoingConverter
-    protected fun outConvStmt(@Suppress("UNUSED_PARAMETER") v: Any?): Any? = null
+    protected fun outConvDecl(@Suppress("UNUSED_PARAMETER") v: Any?): Any? = null
 
     @TruffleBoundary
     override fun getSourceSection(): SourceSection {
